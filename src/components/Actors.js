961a8bf2d75e9ddd776061b937d11bc5ef172ecd
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-06-30 07:37:35 -0700

    completes lab

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,22 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      <div>
+        {actors.map((actor) => {
+          return(
+            <div key={actor.name}>
+              <p>{actor.name}</p>
+              <li>{actor.movies}</li>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,21 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      <div>
+        {directors.map((director) => {
+          return (
+            <div key={director.name}>
+              <p>{director.name}</p>
+              <li>{director.movies}</li>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Directors;
